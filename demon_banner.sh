d7518b232b1a05b6ac710fe88c988e39198eb559
byfurkan00 <135124958+byfurkan00@users.noreply.github.com>
2026-04-05 19:33:57 +0300

    Update demon_banner.sh

--- demon_banner.sh
+++ demon_banner.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 clear
-echo -e ""
+# Yazıyı KIRMIZI yapar
+echo -e "\e[1;31m"
 echo "  ____ __   __  _____ _   _ ____  _  __  _    _   _ "
 echo " | __ ) \ / / |  ___| | | |  _ \| |/ / / \  | \ | |"
 echo " |  _ \  V /  | |_  | | | | |_) |   / / _ \ |  \| |"
 echo " | |_) | | |  |  _| | |_| |  _ <|  \ / ___ \| |\  |"
 echo " |____/  |_|  |_|    \___/|_| \_\_|\_/_/   \_\_| \_|"
-echo -e ""
+# Renkleri normale döndürür
+echo -e "\e[0m"
+
+# 2 saniye bekler ve ekranı temizler
+sleep 2
+clear
